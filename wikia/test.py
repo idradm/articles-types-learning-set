e98f0fb5939d0c0a596ea327c8bb8b659539d8d1
jacekjursza <jacek@wikia-inc.com>
2014-07-11 17:23:24 +0200

    adding lang

--- wikia/test.py
+++ wikia/test.py
@@ -1,10 +1,11 @@
 #s = search.WikiaSearch()
 #resp = s.query('ns:0', 0, 10, 'random_1+asc')
 #print resp['response']['docs']
+import api, search
 
-
-#a = api.DocumentProvider(search.WikiaSearch())
-#resp = a.generate_new_sample(10)
+a = api.DocumentProvider(search.WikiaSearch())
+a.set_hub_filter("Entertainment,Gaming")
+resp = a.generate_new_sample(10)
 
 
 
